7d95be0b02fcf28a3ec4ba009dc5140bccd8d79f
naoki-kkc <naoki2kuchi@gmail.com>
2020-10-22 17:08:07 +0900

    first commit - all lesson end

---
+++ MyBallGame/Assets/Scripts/BallFactroryScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallFactroryScript : MonoBehaviour
+{
+
+    public GameObject ball;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Instantiate(ball, transform.position, transform.rotation);
+        InvokeRepeating("SpawnBall", 0f, 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void SpawnBall()
+    {
+        float randomX = Random.Range(-5f, 5f);
+        Instantiate(ball, new Vector3(randomX, transform.position.y, transform.position.z), transform.rotation);
+    }
+}
---
+++ MyBallGame/Assets/Scripts/PaddleScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleScript : MonoBehaviour
+
+{
+    // Unity側から書き換え可能になるようにprivateからpublicへ変更
+    //private float speed;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 外部から値が与えられた後でStartが呼ばれる動きをしているので、コメントアウトえ
+        //speed = 5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // キー入力を監視し、パドルを動かす
+        transform.position += new Vector3(Input.GetAxis("Horizontal") * Time.deltaTime * speed , 0f, 0f);
+        //Debug.Log(transform.position.x);
+    }
+}
